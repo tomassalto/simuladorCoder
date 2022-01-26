8d2684385a738aac5282d91e72b47fbacd87c113
tomassalto <tomasagustinsalto@gmail.com>
2022-01-25 22:21:27 -0300

    Update tomasSalto.js
    update final

--- js/tomasSalto.js
+++ js/tomasSalto.js
@@ -48,8 +48,12 @@ function mostrarMenu(numero){
          "6. GEFORCE NVIDIA 3080"+"\n"+
          "7. GEFORCE NVIDIA 3080TI");
       
-      opcion = prompt("Desea ingresar una opcion?: (si/no)");
+      opcion = prompt("Desea ingresar una opcion?: (si/no), (*) para salir");
 
+      if(opcion == "no"){
+
+         break
+      }   
       if(opcion == "si"){
 
          numero = prompt("Ingrese el numero correspondiente para ver su precio: ");  
@@ -61,22 +65,25 @@ function mostrarMenu(numero){
          }
          if(numero >= 1 && numero <=7){
 
-            opcion = prompt("Desea ver otra vez el menú? (si/no)");               
-               
+            opcion = prompt("Desea ver otra vez el menú? (si/no), (*) para salir");               
+               if(opcion == "no" || opcion == "*"){
+                  break
+               }
          }else{
 
             window.alert("Ingrese una opcion correcta!");
             numero = prompt("Ingrese una opcion: ");
                               
          }                    
-       
-      }else{
-         window.alert("Adios!");
       }
-   }while(numero >= 1 && numero <= 7);
+         
+      
+   }while(opcion != "*");{
+      window.alert ("Adios vuelva pronto!");
+   }
 }   
 
-opcion = prompt("Desea ver el menu de compras? (si/no): ");
+opcion = prompt("Desea ver el menu de compras?: (si/no)");
 
 if(opcion == "si"){
 

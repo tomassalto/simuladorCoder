2c03c7f1d2093fd9587cca154a46d5533c12263a
tomassalto <tomasagustinsalto@gmail.com>
2022-01-31 04:53:15 -0300

    Update tomasSalto.js
    agregue los objetos, cambie menú y switch. Falta los arrays

--- js/tomasSalto.js
+++ js/tomasSalto.js
@@ -1,78 +1,72 @@
-function mostrarPrecios(menuOpciones){
+// function mostrarPrecios(menuOpciones){
+     
+//    switch(menuOpciones){
 
-   precio = "";
+//       case 1:
+//        console.log("El precio de GEFORCE NVIDIA 2060 es de: 145.000 ARS");
+//        break;
 
-   if(menuOpciones == 1){
+//       case 2:
+//        console.log("El precio de GEFORCE NVIDIA 3060 es de: 150.000 ARS");
+//        break;
+//       case 3:
+//        console.log("El precio de GEFORCE NVIDIA 3060TI es de: 180.000 ARS");
+//        break;
 
-      precio = "El precio de GEFORCE NVIDIA 2060 es de: 145.000 ARS";
+//       case 4:
+//        console.log("El precio de GEFORCE NVIDIA 3070 es de: 210.000 ARS");
+//        break;
 
-   }else if(menuOpciones == 2){
+//        case 5:
+//        console.log("El precio de GEFORCE NVIDIA 3070TI es de: 260.000 ARS");
+//        break;
 
-      precio = "El precio de GEFORCE NVIDIA 3060 es de: 150.000 ARS";
+//        case 6:
+//        console.log("El precio de GEFORCE NVIDIA 3080 es de: 315.000 ARS");
+//        break;
 
-   }else if(menuOpciones == 3){
-
-      precio = "El precio de GEFORCE NVIDIA 3060TI es de: 180.000 ARS";
-
-   }else if(menuOpciones == 4){
-
-      precio = "El precio de GEFORCE NVIDIA 3070 es de: 210.000 ARS";
-
-   }else if(menuOpciones == 5){
-
-      precio = "El precio de GEFORCE NVIDIA 3070TI es de: 260.000 ARS";
-      
-   }else if(menuOpciones == 6){
-
-      precio = "El precio de GEFORCE NVIDIA 3080 es de: 315.000 ARS";
-
-   }else if(menuOpciones == 7){
-
-      precio = "El precio de GEFORCE NVIDIA 3080TI es de: 410.000 ARS";
-   }
-   
-   return precio;
-   
-}
+//        case 7:
+//        console.log("El precio de GEFORCE NVIDIA 3080TI es de: 410.000 ARS");
+//        break;
+       
+//        default:
+//           console.log("Error!");
+//           break; 
+//    }
+        
+// }
 
 function mostrarMenu(numero){
 
    do{ 
+      
+      opcion = prompt("Desea ingresar una opcion?: ('si'), ('no' o '*') para salir");
 
-      window.alert("MENU DE PLACAS DE VIDEO:"+"\n"+"\n"+
+      if(opcion == "no"){
+
+         break;
+      }   
+      if(opcion == "si"){
+
+         numero = parseInt(prompt("Ingrese el numero correspondiente para ver las caracteristicas del producto: "+"\n"+"\n"+
+         "MENU DE PLACAS DE VIDEO:"+"\n"+"\n"+
          "1. GEFORCE NVIDIA 2060"+"\n"+
          "2. GEFORCE NVIDIA 3060"+"\n"+
          "3. GEFORCE NVIDIA 3060TI"+"\n"+
          "4. GEFORCE NVIDIA 3070"+"\n"+
          "5. GEFORCE NVIDIA 3070TI"+"\n"+
          "6. GEFORCE NVIDIA 3080"+"\n"+
-         "7. GEFORCE NVIDIA 3080TI");
-      
-      opcion = prompt("Desea ingresar una opcion?: ('si'), ('no' o '*') para salir");
-
-      if(opcion == "no"){
-
-         break
-      }   
-      if(opcion == "si"){
-
-         numero = prompt("Ingrese el numero correspondiente para ver su precio: ");  
+         "7. GEFORCE NVIDIA 3080TI"));  
          
          if(numero >= 1 && numero <= 7){
       
-            console.log(mostrarPrecios(numero));
+            mostrarDatos(numero);
                      
-         }
-         if(numero >= 1 && numero <=7){
-
-            opcion = prompt("Desea ver otra vez el menú? ('si'), ('no' o '*') para salir");               
-               if(opcion == "no" || opcion == "*"){
-                  break
-               }
-         }else{
+         } 
+         else{
 
             window.alert("Ingrese una opcion correcta!");
-            numero = prompt("Ingrese una opcion: ");
+            
                               
          }                    
       }
@@ -83,11 +77,117 @@ function mostrarMenu(numero){
    }
 }   
 
+//objetos
+
+// const placaDeVideo1 = {
+
+//    nombre : "Nvidia Geforce 2060",
+//    ram : "8gb",
+//    precio : "145.000 ars",
+
+// }
+
+// console.log (placaDeVideo1);
+// console.log (placaDeVideo1.nombre);
+// console.log (placaDeVideo1.ram);
+// console.log (placaDeVideo1.precio);
+
+// console.log (placaDeVideo1["nombre"]);
+// console.log (placaDeVideo1["ram"]);
+// console.log (placaDeVideo1["precio"]);
+
+// placaDeVideo1.marca = "Zotac";
+// console.log(placaDeVideo1.marca);
+
+// function placaDeVideo(nombre, ram, precio){
+
+//    this.nombre = nombre;
+//    this.ram = ram;
+//    this.precio = precio;
+// }
+
+// const placaDeVideo1 = new placaDeVideo("Nvidia Geforce 2060","8GB","145.000 ARS");
+// console.log(placaDeVideo1);
+// console.log(placaDeVideo1.nombre);
+// console.log(placaDeVideo1.ram);
+// console.log(placaDeVideo1.precio);
+
+function mostrarDatos(menuOpciones){
+
+   switch(menuOpciones){
+
+      case 1:
+       console.log(placaDeVideo1);
+       break;
+
+      case 2:
+       console.log(placaDeVideo2);
+       break;
+      case 3:
+       console.log(placaDeVideo3);
+       break;
+
+      case 4:
+       console.log(placaDeVideo4);
+       break;
+
+       case 5:
+       console.log(placaDeVideo5);
+       break;
+
+       case 6:
+       console.log(placaDeVideo6);
+       break;
+
+       case 7:
+       console.log(placaDeVideo7);
+       break;
+       
+       default:
+          console.log("Error!");
+          break; 
+   }
+}
+
+class placaDeVideo{
+
+   constructor(nombre, ram, precio, marca){
+
+      this.nombre = nombre;
+      this.ram = ram;
+      this.precio = precio;
+      this.marca = marca;
+   }
+}
+
+const placaDeVideo1 = new placaDeVideo ("Nvidia Geforce 2060","6GB","150.000 ars","Zotac" );
+
+const placaDeVideo2 = new placaDeVideo ("Nvidia Geforce 3060","12GB","145.000 ars","Zotac");
+
+const placaDeVideo3 = new placaDeVideo ("Nvidia Geforce 3060TI","8GB","180.000 ars","PNY");
+
+const placaDeVideo4 = new placaDeVideo ("Nvidia Geforce 3070","8GB","210.000 ars","Zotac");
+
+const placaDeVideo5 = new placaDeVideo ("Nvidia Geforce 3070TI","8GB","260.000 ars","Zotac");
+
+const placaDeVideo6 = new placaDeVideo ("Nvidia Geforce 3080","10GB","347.000 ars","MSI");
+
+const placaDeVideo7 = new placaDeVideo ("Nvidia Geforce 3080TI","12GB","410.000 ars","PYN");
+
 opcion = prompt("Desea ver el menu de compras?: (si/no)");
 
 if(opcion == "si"){
+   
+   window.alert("MENU DE PLACAS DE VIDEO:"+"\n"+"\n"+
+   "1. GEFORCE NVIDIA 2060"+"\n"+
+   "2. GEFORCE NVIDIA 3060"+"\n"+
+   "3. GEFORCE NVIDIA 3060TI"+"\n"+
+   "4. GEFORCE NVIDIA 3070"+"\n"+
+   "5. GEFORCE NVIDIA 3070TI"+"\n"+
+   "6. GEFORCE NVIDIA 3080"+"\n"+
+   "7. GEFORCE NVIDIA 3080TI");
 
-   console.log(mostrarMenu(opcion));
+   mostrarMenu(opcion);
    
          
 }else{
